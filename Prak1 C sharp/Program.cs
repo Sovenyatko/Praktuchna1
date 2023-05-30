187715855f3eac7b93c551c356eb68ed1ee573af
Sovenyatko <alexandryshka@ukr.net>
2023-05-31 01:43:17 +0300

    prak1

---
+++ Prak1 C sharp/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Prak1_C_sharp
+{ 
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введiть число:");
+        string input = Console.ReadLine();
+
+        int minDigit = int.MaxValue;
+        int maxDigit = int.MinValue;
+
+        foreach (char c in input)
+        {
+            if (char.IsDigit(c))
+            {
+                int digit = c - '0';
+                if (digit < minDigit)
+                {
+                    minDigit = digit;
+                }
+                if (digit > maxDigit)
+                {
+                    maxDigit = digit;
+                }
+            }
+        }
+
+        Console.WriteLine($"Мiнiмальна цифра: {minDigit}");
+        Console.WriteLine($"Максимальна цифра: {maxDigit}");
+
+        Console.ReadLine();
+    }
+}
+}
---
+++ Prak1.1 C sharp/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Prak1._1_C_sharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double y = 0;
+            Console.WriteLine("Enter x: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            if (x < -1)
+
+            {
+                y = (x - 1) / (x + 1);
+            }
+            else
+            if (x < 20)
+            {
+                for (int i = -1; i <= x; i++)
+                {
+                    y += 4 - (i / 2);
+                }
+
+            }
+
+            else 
+            {
+
+                y = 1;
+
+                for (int i = 20; i <= x; i++)
+                {
+                    y *= 0.5 * (i / 3.0 + 1.0);
+                }
+            };
+
+            Console.WriteLine(y);
+        }
+    }
+}
